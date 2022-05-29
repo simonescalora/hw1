7397db7b7c9853ebd41f93a601b5ac8b52de6f9d
simonescalora <102426303+simonescalora@users.noreply.github.com>
2022-05-29 17:10:40 +0200

    Add files via upload

--- db.sql
+++ db.sql
@@ -1,22 +1,22 @@
-CREATE DATABASE IF NOT EXISTS `compito`;
-USE `compito`;
+CREATE DATABASE IF NOT EXISTS 'hw1';
+USE 'hw1';
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `cognome` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS 'users' (
+  'id' int NOT NULL AUTO_INCREMENT,
+  'nome' varchar(255) NOT NULL,
+  'cognome' varchar(255) NOT NULL,
+  'username' varchar(255) NOT NULL,
+  'password' varchar(255) NOT NULL,
+  'email' varchar(255) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `cart_item` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `price` float NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `category_id` int NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS 'cart_item' (
+  'id' int NOT NULL AUTO_INCREMENT,
+  'name' varchar(255) NOT NULL,
+  'description' varchar(255) NOT NULL,
+  'price' float NOT NULL,
+  'image' varchar(255) NOT NULL,
+  'category_id' int NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB;
